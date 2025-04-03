a997000aacc4c333c3a721e345a30ceff8b0cc1e
NV23023 <mohd.hyy17@gmail.com>
2025-04-03 09:24:34 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+function getComputerChoice() {
+    // Generate a random number between 0 and 1
+    const randomNum = Math.random();
+    
+    // Return "rock", "paper", or "scissors" based on the random number
+    if (randomNum < 0.33) {
+        return "rock";
+    } else if (randomNum < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// Test the function
+console.log(getComputerChoice()); // Randomly logs "rock", "paper", or "scissors"
